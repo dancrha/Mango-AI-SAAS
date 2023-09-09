1c0baccd7388783c5eadaa82daab77b85381171a
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-09 13:51:33 -0400

    prisma set up

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -4,6 +4,7 @@ import OpenAI from "openai";
 import ChatCompletionRequestMessage from "openai";
 import Configuration from "openai";
 import OpenAIApi from "openai";
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -33,11 +34,18 @@ export async function POST(req: Request) {
     if (!messages) {
       return new NextResponse("Messages are required.", { status: 400 });
     }
+    const freeTrial = await checkApiLimit();
+
+    if (!freeTrial) {
+      return new NextResponse("Free trial has expired", { status: 403 });
+    }
 
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [instructionMessage, ...messages],
     });
+
+    await increaseApiLimit();
 
     return NextResponse.json(response.choices[0].message);
   } catch (error) {
